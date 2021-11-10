857ad72e53f9910e5fae539b8b09a83d74422b5e
Lareta064 <lareta064@gmail.com>
2021-11-10 21:49:45 +0300

    header-bottom

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hi!');
+/*========== Показать телефон на фиксированном меню ==========*/
+window.addEventListener('scroll', ()=>{
+	if(window.pageYOffset > 80){
+		document.querySelector('.header').classList.add('header-fixed');
+	}else{
+		document.querySelector('.header').classList.remove('header-fixed');
+	}
+	
+})
