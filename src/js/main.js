5a6979ef393884aad0106e6c2fafdf60395ae095
Lareta064 <lareta064@gmail.com>
2021-11-20 11:55:21 +0300

    rating-block

--- src/js/main.js
+++ src/js/main.js
@@ -477,3 +477,15 @@ if(reviewItems.length > 0){
     })
   }
 }
+/*=========RATING BLOCK =======*/
+const ratingBlock = document.querySelectorAll('[data-rating]');
+if(ratingBlock){
+	ratingBlock.forEach( function(item, index) {
+		const itemActive = item.querySelector('[data-count]');
+		const itemActiveCount = itemActive.querySelectorAll('i');
+		const itemActiveLength = itemActiveCount.length
+		const itemActiveVal = itemActive.getAttribute('data-count');
+		const activeBlockWidth = +(((itemActiveVal / itemActiveLength) * 100)+0.2) ;//0.5 погрешность нп расстояние между звездами
+		itemActive.style.width =`${activeBlockWidth}%`;
+	});
+}
